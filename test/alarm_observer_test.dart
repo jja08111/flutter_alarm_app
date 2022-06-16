63567a51b0839a4c701024ccd65214986fed95ed
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-06-17 00:02:53 +0900

    Create alarm_observer_test.dart

---
+++ test/alarm_observer_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_alarm_app/provider/alarm_list_provider.dart';
+import 'package:flutter_alarm_app/provider/alarm_state.dart';
+import 'package:flutter_alarm_app/view/alarm_observer.dart';
+import 'package:flutter_alarm_app/view/alarm_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+import 'fake_alarm.dart';
+
+void main() {
+  testWidgets('shows AlarmScreen if alarm is fired', (tester) async {
+    final AlarmState alarmState = AlarmState();
+    final AlarmListProvider alarmList = AlarmListProvider([fakeAlarm]);
+
+    await tester.pumpWidget(MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AlarmState>(create: (_) => alarmState),
+        ChangeNotifierProvider<AlarmListProvider>(create: (_) => alarmList),
+      ],
+      child: const MaterialApp(
+        home: AlarmObserver(
+          child: Text('Home screen'),
+        ),
+      ),
+    ));
+
+    expect(find.byType(AlarmScreen), findsNothing);
+
+    alarmState.fire(fakeAlarm.id);
+    await tester.pump();
+
+    expect(find.byType(AlarmScreen), findsOneWidget);
+  });
+}
